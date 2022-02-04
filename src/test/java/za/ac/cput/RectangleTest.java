60c19e2b0cafb3f95dda410569877b33d8bf0dd8
Tarren-Marc Adams <you@example.com>
2022-02-04 20:47:41 +0200

    Test class

---
+++ src/test/java/za/ac/cput/RectangleTest.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Tarren-Marc Adams
+ * student number 214041794
+ *
+ */
+
+
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}

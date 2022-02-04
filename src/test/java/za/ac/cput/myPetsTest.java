adf6f684e7108ddba4f9d452dbdbf5f1444b3c7c
breySean <breytonernstzen@gmail.com>
2022-02-04 18:17:17 +0200

    test file content removed

--- src/test/java/za/ac/cput/myPetsTest.java
+++ src/test/java/za/ac/cput/myPetsTest.java
@@ -14,39 +14,9 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 class myPetsTest {
-        private myPets myPets1;
-        private myPets myPets2;
-        private myPets myPets3;
 
     @BeforeEach
     void setUp() {
-        //First step
-        myPets1 = new myPets("Milo","Jack russel","Dog",4);
-        myPets2 = new myPets("Jango","Pitbull","Dog",4);
-        myPets3 = myPets1;
-    }
-
-    @Test
-    @Timeout(value = 100,unit = TimeUnit.MILLISECONDS)
-    void testEquality(){
-        //Test for object equality
-        //pass test
-        assertEquals(myPets1,myPets3);
-        assertEquals(myPets1,myPets1);
-
-        //assertEquals("Rango",myPets1.getName()); Deliberate test failure
 
     }
-    @Test
-    @Timeout(value = 100,unit = TimeUnit.MILLISECONDS)
-    @Disabled
-    void testIdentity(){
-        //Test for object identity
-        //This test has been disabled
-        assertSame(myPets1,myPets3);
-
-        assertNotSame(myPets1,myPets2);
-        assertNotSame(myPets3,myPets2);
-    }
-
-    }
+}

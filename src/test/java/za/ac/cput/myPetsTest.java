b680ce688c759631099db123b57be6427d3f680d
breySean <breytonernstzen@gmail.com>
2022-02-04 16:32:58 +0200

    timeouts added,test identity performed,and disabled test for identity

--- src/test/java/za/ac/cput/myPetsTest.java
+++ src/test/java/za/ac/cput/myPetsTest.java
@@ -5,7 +5,11 @@ package za.ac.cput;
     4 February 2022
  */
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,16 +21,32 @@ class myPetsTest {
     @BeforeEach
     void setUp() {
         //First step
-        myPets1 = new myPets();
-        myPets2 = new myPets();
+        myPets1 = new myPets("Milo","Jack russel","Dog",4);
+        myPets2 = new myPets("Jango","Pitbull","Dog",4);
         myPets3 = myPets1;
     }
 
     @Test
+    @Timeout(value = 100,unit = TimeUnit.MILLISECONDS)
     void testEquality(){
         //Test for object equality
+        //pass test
         assertEquals(myPets1,myPets3);
         assertEquals(myPets1,myPets1);
 
+        //assertEquals("Rango",myPets1.getName()); Deliberate test failure
+
     }
-}
+    @Test
+    @Timeout(value = 100,unit = TimeUnit.MILLISECONDS)
+    @Disabled
+    void testIdentity(){
+        //Test for object identity
+        //This test has been disabled
+        assertSame(myPets1,myPets3);
+
+        assertNotSame(myPets1,myPets2);
+        assertNotSame(myPets3,myPets2);
+    }
+
+    }

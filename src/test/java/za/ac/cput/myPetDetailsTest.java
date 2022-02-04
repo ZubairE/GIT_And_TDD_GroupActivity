63381bd23815ed51fccdbb6436035b88eac401e7
breytonE <80263861+breytonE@users.noreply.github.com>
2022-02-04 21:16:05 +0200

    Student Details added in comment section

--- src/test/java/za/ac/cput/myPetDetailsTest.java
+++ src/test/java/za/ac/cput/myPetDetailsTest.java
@@ -1,5 +1,11 @@
 package za.ac.cput;
 
+/*
+    Breyton Ernstzen - 217203027
+    GIT and TDD group excercise
+    4 February 2022
+ */
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;

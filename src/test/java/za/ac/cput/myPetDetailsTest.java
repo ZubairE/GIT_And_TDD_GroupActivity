5607cece71dc3e534fa71d68813f08604ffd3ea0
breySean <breytonernstzen@gmail.com>
2022-02-04 16:42:20 +0200

    Classes has been imported into tests class

---
+++ src/test/java/za/ac/cput/myPetDetailsTest.java
@@ -0,0 +1,16 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class myPetDetailsTest {
+    private myPets myPets1;
+    private myPets myPets2;
+    private myPets myPets3;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}

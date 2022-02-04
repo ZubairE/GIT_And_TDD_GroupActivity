56516de3c4ace71a8bcfdabbd7f495c33c1bce68
ZubairE <esau.zubair98@gmail.com>
2022-02-04 16:06:17 +0200

    Creating testing methods

--- src/test/java/za/ac/cput/NewCarTest.java
+++ src/test/java/za/ac/cput/NewCarTest.java
@@ -1,12 +1,35 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewCarTest {
 
+    private NewCar car1;
+    private NewCar car2;
+
+
     @BeforeEach
     void setUp() {
+
+    car1 = new NewCar();
+    car2 = new NewCar();
+
     }
+
+
+   @Test
+   void testIdentity(){
+
+        assertSame(car1, car2);
+
+   }
+
+   @Test
+   void testEquality(){
+
+   }
+
 }

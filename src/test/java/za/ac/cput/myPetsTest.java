04ca342dc276d63f572d378ce1e93f404492ff22
breySean <breytonernstzen@gmail.com>
2022-02-04 15:39:51 +0200

    setUp method created

--- src/test/java/za/ac/cput/myPetsTest.java
+++ src/test/java/za/ac/cput/myPetsTest.java
@@ -1,11 +1,19 @@
 package za.ac.cput;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class myPetsTest {
+        private myPets myPets1;
+        private myPets myPets2;
+        private myPets myPets3;
 
-    @org.junit.jupiter.api.BeforeEach
-
+    @BeforeEach
     void setUp() {
+        //First step
+        myPets1 = new myPets();
+        myPets2 = new myPets();
+        myPets3 = myPets1;
     }
 }

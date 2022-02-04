f23b5cd521250647b13e137894cdaf5088c03acc
Tarren-Marc Adams <you@example.com>
2022-02-04 21:01:35 +0200

    Rectangle Equality Test

--- src/test/java/za/ac/cput/RectangleTest.java
+++ src/test/java/za/ac/cput/RectangleTest.java
@@ -9,14 +9,29 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
+    private Rectangle rectangle1;
+    private Rectangle rectangle2;
+    private Rectangle rectangle3;
+    private Rectangle rectangle4;
+    private Rectangle rectangle5;
 
     @BeforeEach
     void setUp() {
-
+        rectangle1 = new Rectangle();
+        rectangle2 = new Rectangle();
+        rectangle3 = rectangle1;
+        rectangle4 = new Rectangle();
+        rectangle5 = rectangle2;
+    }
+    @Test
+    public void testEquality(){
+        assertEquals(rectangle1,rectangle3);
+        assertEquals(rectangle2,rectangle5);
     }
 }

0171378e880ff3ba38bb05ee10ef5334cb0e7b62
ZubairE <esau.zubair98@gmail.com>
2022-02-04 16:13:30 +0200

    Testing Equality

--- src/test/java/za/ac/cput/NewCarTest.java
+++ src/test/java/za/ac/cput/NewCarTest.java
@@ -9,13 +9,14 @@ class NewCarTest {
 
     private NewCar car1;
     private NewCar car2;
-
+    private NewCar car3;
 
     @BeforeEach
     void setUp() {
 
     car1 = new NewCar();
     car2 = new NewCar();
+    car3 = car1 ;
 
     }
 
@@ -23,13 +24,13 @@ class NewCarTest {
    @Test
    void testIdentity(){
 
-        assertSame(car1, car2);
-
+        assertSame(car1, car3);
    }
 
    @Test
    void testEquality(){
 
+        assertEquals(car1, car2);
    }
 
 }

37deef5dd50a111bda125f728d0981f4854a43c9
breySean <breytonernstzen@gmail.com>
2022-02-04 16:47:42 +0200

    Test for object equality has been performed.Also timeouts has been done.

--- src/test/java/za/ac/cput/myPetDetailsTest.java
+++ src/test/java/za/ac/cput/myPetDetailsTest.java
@@ -1,6 +1,11 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,4 +21,17 @@ class myPetDetailsTest {
         myPets2 = new myPets("Jango","Pitbull","Dog",4);
         myPets3 = myPets1;
     }
-}
+
+    @Test
+    @Timeout(value = 100,unit = TimeUnit.MILLISECONDS)
+    void testEquality() {
+        //Test for object equality
+        //pass test
+        assertEquals(myPets1, myPets3);
+        assertEquals(myPets1, myPets1);
+
+        //assertEquals("Rango",myPets1.getName()); Deliberate test failure
+    }
+
+
+    }

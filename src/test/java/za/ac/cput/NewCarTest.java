77b76da21f248e6d4405d85593cbf965f3619b1e
ZubairE <esau.zubair98@gmail.com>
2022-02-04 15:43:41 +0200

    Creating car test class

---
+++ src/test/java/za/ac/cput/NewCarTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewCarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
